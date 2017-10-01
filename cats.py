2e935725f9d3797084b0e3efdaa3a443308f81c5
Enio Fernandes <ef@cic.ac>
2017-10-01 10:48:56 +0000

    first commit

---
+++ cats.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+#%matplotlib inline
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
